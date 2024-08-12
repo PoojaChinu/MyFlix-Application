9e1fb648c4427a258d07c22c1a38c209ccc18f8d
PoojaChinu <166599982+PoojaChinu@users.noreply.github.com>
2024-08-12 10:51:54 +0200

    updating local host for angular

--- index.js
+++ index.js
@@ -37,6 +37,7 @@ const cors = require("cors");
 let allowedOrigins = [
   "http://localhost:8080",
   "http://localhost:1234",
+  "http://localhost:4200",
   "https://pooja-porwal-myflix.netlify.app",
 ];
 
